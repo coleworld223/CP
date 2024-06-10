91fd190b6f80d84a594402fe2bae26b0890014a7
anuj <anujanand223@gmai.com>
2024-06-10 12:20:06 +0530

    added cses dp's

---
+++ DP/CSES_DP/Projects.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+void solve () {
+    cout << pow (3, 20) << '\n';
+}
+
+int main () {
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int tests = 1;
+    // cin >> tests;
+
+    for (int i = 0; i < tests; i += 1) {
+        solve ();
+    }
+}
